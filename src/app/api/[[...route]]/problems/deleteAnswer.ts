ebe708b5dfd72374bd4a35ce936b53e6a4ad4154
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2026-02-19 17:21:31 +0900

    Feat✨: deleteAnswer

---
+++ src/app/api/[[...route]]/problems/deleteAnswer.ts
@@ -0,0 +1,40 @@
+import { prisma } from "@/lib/prisma";
+import { zValidator } from "@hono/zod-validator";
+import { createFactory } from "hono/factory";
+import type { Session } from "next-auth";
+import { z } from "zod";
+import { withAdmin } from "~/middleware/auth";
+
+type Variables = {
+  session: Session;
+};
+
+const factory = createFactory<{ Variables: Variables }>();
+
+export const deleteAnswer = factory.createHandlers(
+  withAdmin,
+  zValidator(
+    "param",
+    z.object({
+      answer_id: z.string().cuid(),
+    }),
+  ),
+  async (c) => {
+    const { answer_id } = c.req.valid("param");
+
+    try {
+      await prisma.answer.delete({
+        where: { id: answer_id },
+      });
+      return c.body(null, 204);
+    } catch (error) {
+      console.error("Error deleting answer:", error);
+      return c.json(
+        {
+          error: "解答の削除中にエラーが発生しました",
+        },
+        500,
+      );
+    }
+  },
+);
