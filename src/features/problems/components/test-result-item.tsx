19908abb419de07d790be46d78afb4a313360845
sakashita aoi <tububu1230aoi@gmail.com>
2026-03-14 21:15:55 +0900

    [Feat✨] コンソールの「実際の出力」「エラー」をResizableにする #186

--- src/features/problems/components/test-result-item.tsx
+++ src/features/problems/components/test-result-item.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Textarea } from "@/components/ui/textarea";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import type { ConvertDateToString } from "@/types/utils";
